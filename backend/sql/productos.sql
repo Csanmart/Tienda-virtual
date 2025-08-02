bdb7ed4d3d9b0ba2362fde34d11be909fd9c8639
Csanmart <csanmart2024@hotmail.com>
2025-08-02 14:45:34 -0500

    Creamos bases de datos

---
+++ backend/sql/productos.sql
@@ -0,0 +1,22 @@
+-- Active: 1748126598734@@127.0.0.1@3306
+CREATE DATABASE productos
+--Exportacion de base de datos
+
+USE productos
+
+
+
+--Tabla de productos
+CREATE TABLE productos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT,
+    categoria_id INT,
+    imagen BLOB NOT NULL,
+    nombre VARCHAR(455) NOT NULL,
+    precio DECIMAL(10, 0),
+    descripcion TEXT NOT NULL,
+    cantidad INT NOT NULL,
+    estado ENUM('Disponible', 'Vendido') DEFAULT 'Disponible',
+    tipo_oferta ENUM('Donacion', 'Venta'),
+    fecha_publicacion DATETIME DEFAULT CURRENT_TIMESTAMP
+);
