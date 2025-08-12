0d03a38e719c3126c2dbf42ddf8a09a9a03f0628
Csanmart <csanmart2024@hotmail.com>
2025-08-11 23:05:32 -0500

    Completamos el ver mas

--- frontend/js/components/cliente.js
+++ frontend/js/components/cliente.js
@@ -1,49 +1,65 @@
-const tarjeta = document.getElementById('tarjeta');
-const apiProductos = 'http://localhost:3000/productos/productos'
-const strong = document.getElementById('nombre')
-const api = 'http://localhost:3000/api/usuarios';
+const strongName = document.getElementById("nombre");
+const strong = document.getElementById("email");
+const api = "http://localhost:3000/api/usuarios";
 
+fetch(api, { method: "GET" })
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((data) => {
+      strongName.innerHTML = `${data.name}`;
+      strong.innerHTML = `${data.email}`;
+    });
+  });
 
+//Tarjeta de productos
+const tarjeta = document.getElementById("tarjeta");
+const apiProductos = "http://localhost:3000/productos/productos";
 
-
-
-fetch(api, {method: 'GET'})
-.then(response => response.json())
-.then(data => {
-  data.forEach(data =>{
-    strong.innerHTML = `${data.name}`
-  })
-})
-
-fetch(apiProductos, {method:'GET'})
-.then(response => {
-    if(!response.ok){
-        throw new Error('Error en el response', response);
+fetch(apiProductos, { method: "GET" })
+  .then((response) => {
+    console.log(response)
+    if (!response.ok) {
+      throw new Error("Error en el response", response);
     }
     return response.json();
-})
-.then(data => {
-    data.forEach(producto =>{
-        const card = document.createElement('div');
-        
+  })
+  .then((data) => {
+    data.forEach((producto) => {
+      const card = document.createElement("div");
 
-        card.innerHTML=  `
+      card.innerHTML = `
         <div class="card h-100 shadow-sm">
-          <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}" style="object-fit: cover; height: 200px;">
+          <img src="${producto.imagen}" class="card-img-top" alt="${
+        producto.nombre
+      }" style="object-fit: cover; height: 200px;">
           <div class="card-body d-flex flex-column">
             <h5 class="card-title">${producto.nombre}</h5>
             <p class="card-text text-muted">${producto.descripcion}</p>
             <div class="mt-auto">
               <p class="fw-bold text-success">$${producto.precio}</p>
               <button class="btn btn-primary w-100 mb-3">Comprar</button>
+              <button class= "btn btn-secundary mb-3 w-100" onclick="verMas(${producto.id})"><a>Ver mas</a></button>
             </div>
-            <button class"btn btn-primary mb-3"><a>Ver mas</a></button>
           </div>
         </div>
         `;
-        tarjeta.appendChild(card)
+      tarjeta.appendChild(card);
     });
-})
-.catch(error =>{
-    console.error('Error obteniendo los datos..', error)
+  })
+  .catch((error) => {
+    console.error("Error obteniendo los datos..", error);
+  });
+
+//Mirar el productos por id
+
+function verMas(id){
+  window.location.href = `ver-mas.html?id=${id}`
+};
+
+const cerrar = document.getElementById('cerrar')
+
+cerrar.addEventListener('click', function(event){
+    if(cerrar){
+        window.location.href = 'login.html'
+    }
 })
